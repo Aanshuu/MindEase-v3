65a0673da76b8d06822d6da7b377710c0a11e536
Aanshuu <e21cseu0085@bennett.edu.in>
2024-09-02 23:35:05 +0530

    Landing Page Created, Page transition added, Button added from chadcn.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils"
+import Navbar from "@/components/common/Navbar";
+// import ClientProviders from "../components/ClientProvider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn('min-h-screen font-sans antialiased', inter.className)}>
+      {/* <ClientProviders> */}
+        <Navbar/>
+        {children}
+      {/* </ClientProviders> */}
+      </body>
     </html>
   );
 }
