96132ad9d3adeb70d06135a45c2aeac9c3966cf0
Aanshuu <e21cseu0085@bennett.edu.in>
2024-09-10 20:12:16 +0530

    redirect to signUp page for un authenticated users, better chat ui, added login button.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,28 @@
 "use client";
+
 import { useState, useEffect, useRef } from "react";
 import { useCurrentUser, pb } from "@/lib/pocketbase";
 import { RecordModel } from "pocketbase";
 import { MoveUp } from "lucide-react";
 import MaxWidthWrapper from "@/components/common/MaxWidthWrapper";
+import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
-export default function Dashboard() {
+export default function DashboardPage() {
+  const router = useRouter();
+  useEffect(() => {
+    if (!pb.authStore.isValid) {
+      redirect("/signUp");
+    }
+  }, [router]);
+
+  const user = pb.authStore.isValid ? pb.authStore.model : null;
+
+  return user? <Dashboard user={user} /> : null;
+}
+
+function Dashboard({ user }: { user: any }) {
+  const router = useRouter();
   const currentUser = useCurrentUser();
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<RecordModel[]>([]);
@@ -49,41 +66,69 @@ export default function Dashboard() {
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       event.preventDefault(); // Prevent form submission if inside a form
       handleSendMessage();
     }
   };
+  useEffect(() => {
+    messageInputRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  const handleLogout = async () => {
+    pb.authStore.clear();
+    router.push("/");
+  };
 
   return (
-    <MaxWidthWrapper>
-      <div className="flex flex-col h-screen">
-        <div className="flex-grow p-4 overflow-y-auto">
-          {messages.map((msg, index) => (
-            <div key={index} className="mb-2">
-              <p className="font-semibold text-black dark:text-white">{msg.sender}</p>
-              <p className="text-black dark:text-white">{msg.text}</p>
-            </div>
-          ))}
-        </div>
-        <div className="p-4 border-t border-black dark:border-white flex flex-col sticky bottom-0">
-          <input
-            type="text"
-            value={message}
-            ref={messageInputRef}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={handleKeyDown}
-            className="w-full p-4 border border-white dark:border-black rounded-3xl bg-black text-white dark:bg-white dark:text-black"
-            placeholder="Type a message..."
-          />
-          <button
-            onClick={handleSendMessage}
-            className="absolute right-6 top-1/2 transform -translate-y-1/2 bg-white text-black dark:bg-black dark:text-white p-2 rounded-full"
-          >
-            <MoveUp/>
-          </button>
-        </div>
+    <>
+      <div>
+        <button
+          className="p-2 text-white bg-black dark:text-black dark:bg-white rounded-md"
+          onClick={handleLogout}
+        >
+          LogOut
+        </button>
       </div>
-    </MaxWidthWrapper>
+
+      <MaxWidthWrapper>
+        <div className="flex flex-col h-screen">
+          <div className="flex-grow p-4 overflow-y-auto">
+            {messages
+              .slice()
+              .reverse()
+              .map((msg, index) => (
+                <div
+                  key={index}
+                  className="mb-2 p-2 bg-black dark:bg-white rounded-lg"
+                >
+                  <p className="font-semibold z-10 text-white dark:text-black">
+                    {msg.sender}
+                  </p>
+                  <p className="z-10 text-white dark:text-black">{msg.text}</p>
+                </div>
+              ))}
+          </div>
+          <div className="p-4 border-t border-black dark:border-white flex flex-col sticky bottom-0">
+            <input
+              type="text"
+              value={message}
+              ref={messageInputRef}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+              className="w-full p-4 border border-white dark:border-black rounded-3xl bg-black text-white dark:bg-white dark:text-black"
+              placeholder="Type a message..."
+            />
+            <button
+              onClick={handleSendMessage}
+              className="absolute right-6 top-1/2 transform -translate-y-1/2 bg-white text-black dark:bg-black dark:text-white p-2 rounded-full"
+            >
+              <MoveUp />
+            </button>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </>
   );
 }
+// export default withAuth(Dashboard, true);
